61fc3ba35a2816739e24618da0e923e38f9b7eb6
Erik Kalkoken <erik.kalkoken@gmail.com>
2023-03-14 15:11:58 +0100

    Update pre-commit

--- utils-test-app/utils_test_app/tests/test_testing.py
+++ utils-test-app/utils_test_app/tests/test_testing.py
@@ -17,7 +17,6 @@ from app_utils.testing import (
 
 class TestNoSocketsTestCase(NoSocketsTestCase):
     def test_raises_exception_on_attempted_network_access(self):
-
         with self.assertRaises(SocketAccessError):
             requests.get("https://www.google.com")
 
