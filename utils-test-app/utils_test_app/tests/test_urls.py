8c3dac20b3e767908c742dd7be8670001973d0d8
ErikKalkoken <erik.kalkoken@gmail.com>
2023-11-07 15:33:33 +0100

    Refactor url tests

--- utils-test-app/utils_test_app/tests/test_urls.py
+++ utils-test-app/utils_test_app/tests/test_urls.py
@@ -1,5 +1,3 @@
-from unittest.mock import patch
-
 from django.test import TestCase, override_settings
 
 from app_utils.urls import reverse_absolute, site_absolute_url
@@ -23,27 +21,23 @@ class TestReverseAbsolute(TestCase):
         self.assertEqual(result, f"{TEST_SITE_URL}/admin/authentication/")
 
 
-@patch(MODULE_PATH + ".settings")
 class TestSiteAbsoluteUrl(TestCase):
-    def test_should_return_absolute_url_for_view(self, mock_settings):
-        # given
-        mock_settings.ESI_SSO_CALLBACK_URL = f"{TEST_SITE_URL}/sso/callback"
+    @override_settings(ESI_SSO_CALLBACK_URL=f"{TEST_SITE_URL}/sso/callback")
+    def test_should_return_absolute_url_for_view(self):
         # when
         result = site_absolute_url()
         # then
         self.assertEqual(result, TEST_SITE_URL)
 
-    def test_should_return_empty_string_1(self, mock_settings):
-        # given
-        mock_settings.ESI_SSO_CALLBACK_URL = TEST_SITE_URL
+    @override_settings(ESI_SSO_CALLBACK_URL=TEST_SITE_URL)
+    def test_should_return_empty_string_1(self):
         # when
         result = site_absolute_url()
         # then
         self.assertEqual(result, "")
 
-    def test_should_return_empty_string_2(self, mock_settings):
-        # given
-        mock_settings.ESI_SSO_CALLBACK_URL = ""
+    @override_settings(ESI_SSO_CALLBACK_URL="")
+    def test_should_return_empty_string_2(self):
         # when
         result = site_absolute_url()
         # then
