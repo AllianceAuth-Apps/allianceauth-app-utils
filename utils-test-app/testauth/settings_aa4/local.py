eb0ef4e7577cc02bae4c39fefdd9f8bb415ab239
ErikKalkoken <erik.kalkoken@gmail.com>
2025-04-14 15:55:54 +0200

    Fix test errors from new AA constraints

--- utils-test-app/testauth/settings_aa4/local.py
+++ utils-test-app/testauth/settings_aa4/local.py
@@ -52,6 +52,7 @@ DATABASES['default'] = {
 ESI_SSO_CLIENT_ID = "dummy"
 ESI_SSO_CLIENT_SECRET = "dummy"
 ESI_SSO_CALLBACK_URL = "http://localhost:8000"
+ESI_USER_CONTACT_EMAIL = "info@example.com"
 
 # By default emails are validated before new users can log in.
 # It's recommended to use a free service like SparkPost or Elastic Email to send email.
